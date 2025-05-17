4059bfd0a3da41cb4858ef718a7ebdfa9c81d015
Umar-Khitab <khitab.khan007@gmail.com>
2025-05-17 16:51:00 +0500

    integrate notification in reward

--- src/reward/reward.module.ts
+++ src/reward/reward.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RewardService } from './reward.service';
 import { RewardController } from './reward.controller';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [RewardController],
   providers: [RewardService],
 })
