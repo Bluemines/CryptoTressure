9988b3dd3f98db0fc17d4db559e8a8b3b778f076
Bilawal <kbilawal84437@gmail.com>
2025-07-02 00:01:59 +0500

    NotificationsModule module fix

--- src/level/level.module.ts
+++ src/level/level.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LevelController } from './level.controller';
 import { LevelService } from './level.service';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [LevelController],
   providers: [LevelService],
 })
